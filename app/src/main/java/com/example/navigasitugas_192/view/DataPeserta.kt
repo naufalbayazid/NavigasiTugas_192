5feb46339d8ce45dd873cbc38a4109ef2a576b2e
naufalbayazid <naufalichwanbayazid@gmail.com>
2025-11-08 20:59:05 +0700

    menambahkan scaffold dan mengatur topBar

--- app/src/main/java/com/example/navigasitugas_192/view/DataPeserta.kt
+++ app/src/main/java/com/example/navigasitugas_192/view/DataPeserta.kt
@@ -38,3 +38,19 @@ fun TampilData(
         Pair(first = stringResource(id = R.string.nama_lengkap), second = "Contoh Nama"),
         Pair(first = stringResource(id = R.string.jenis_kelamin), second = "Lainnya"),
         Pair(first = stringResource(id = R.string.alamt), second = "Yogyakarta")
+    )
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.tampil),
+                        color = Color.White
+                    )
+                },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = colorResource(id = R.color.teal_700)
+                )
+            )
+        }
+    )
